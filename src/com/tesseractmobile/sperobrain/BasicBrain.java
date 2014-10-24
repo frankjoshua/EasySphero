410ccac106d559083c6c08396d34bc3a2098a61f
Joshua Frank <frankjoshua@gmail.com>
2014-10-23 20:48:00 -0500

    Made new angle > 90

--- src/com/tesseractmobile/sperobrain/BasicBrain.java
+++ src/com/tesseractmobile/sperobrain/BasicBrain.java
@@ -5,13 +5,18 @@ import java.util.Random;
 abstract public class BasicBrain implements Brain{
     
     private int heading;
-    private final float velocity = 1f;
+    private final float velocity = .6f;
     
     @Override
     public void onState(final State state) {
         if(Math.abs(state.vx) < 1 && Math.abs(state.vy) < 1){
             final Random random = new Random(System.currentTimeMillis());
-            heading = random.nextInt(360);
+			//Find a heading more that 15 degrees different from current
+			int newHeading = random.nextInt(360);
+			while(Math.abs(newHeading - heading) < 90){
+				newHeading = random.nextInt(360);
+			}
+            heading = newHeading;
             //velocity = random.nextFloat();
         }
         state.velocity = velocity;
